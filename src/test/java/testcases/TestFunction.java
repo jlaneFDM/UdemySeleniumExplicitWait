79f1c02194ed3d16770ce162ee9fd0ee50804ae3
John Lane <john.lane@fdmgroup.com>
2021-12-27 14:26:53 -0500

    completed section on explicit waits

--- src/test/java/testcases/TestFunction.java
+++ src/test/java/testcases/TestFunction.java
@@ -1,12 +1,15 @@
 package testcases;
 
 import java.time.Duration;
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -26,7 +29,12 @@ public class TestFunction {
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		driver.findElement(By.xpath("/html/body/div[1]/a")).click();
 		
-		WebDriverWait wait = new WebDriverWait(driver, 5); //explicit wait
+		//WebDriverWait wait = new WebDriverWait(driver, 5); //explicit wait
+		Wait<WebDriver> wait = new FluentWait<WebDriver>(driver) //fluent wait type of explicit wait
+				.withTimeout(Duration.ofSeconds(10))
+				.pollingEvery(Duration.ofSeconds(2))
+				.withMessage("Timed out after 10 seconds")
+				.ignoring(NoSuchElementException.class);
 		wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.id("login-username")));
 		driver.findElement(By.id("login-username")).sendKeys("jlane7590");
 		driver.findElement(By.id("login-signin")).click();
@@ -35,6 +43,8 @@ public class TestFunction {
 		driver.findElement(By.id("login-signin")).click();
 		
 		System.out.println(driver.findElement(By.className("error-msg")).getText());
+		
+		//note: implicit waits are global, explicit waits are element specific.
 	}
 
 }
